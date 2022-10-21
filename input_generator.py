4cddd81814c3ac29ae9d4261e89c92de0589b374
s-orug <orugants@email.sc.edu>
2022-10-21 19:05:39 -0400

    test

--- input_generator.py
+++ input_generator.py
@@ -4,29 +4,39 @@ from solver import Solver
 
 
 def random_letter_generator(values):
-    letters = [1, 2, 3]
+    letters = [1, 2, 3] # 1 is add, 2 is sub, 3 is multiply
     return random.int
 
 
-low_constrain = 2**8
-high_constrain = 2**16
 
 
 
 for file_number in range(80):
     random_number = random.randint(0, 1)
-    limit = random.randint(low_constrain, 2**20 - 1)
+    limit = random.randint(1, 1024) + 1
 
 
     input_file = open(f'input/input{file_number}.txt', 'w+')
     input_file.write(f"{limit}\n")
 
+    sol = 0
     for _ in range(int(limit/2)):
         i = random.randint(-32768, 32767)
 
         opcode = random.randint(1, 3)
+        opcode = random.randint(1, 3)
+        if opcode == 1:
+            sol = (sol+i) & 65535
+        elif opcode == 2:
+            sol = (sol - i) & 65535
+        else:
+            sol = (sol*i) & 65535
 
         input_file.write(f'{opcode}\n{i}\n')
+    output_file = open(f'output/output{file_number}.txt', 'w+')
+    output_file.write(f"{sol}\n")
+
+    print(file_number)
     input_file.write(f'0')
 
         
